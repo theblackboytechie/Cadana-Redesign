c7142cf85feb47d23e1f6036d53831234c595daa
theblackboytechie <157506948+theblackboytechie@users.noreply.github.com>
2024-12-30 20:06:25 +0100

    updates for pwa
    service worker & manifest.json

--- cadana/resources/js/app.js
+++ cadana/resources/js/app.js
@@ -1,5 +1,13 @@
 import './bootstrap';
 
+if('serviceWorker' in navigator){
+    navigator.serviceWorker.register('Sw.js',  {scope: '/'}).then(function(registration){
+        console.log(`Sw registered successfully`);
+    }).catch(function(registrationError){
+        console.log(`Sw registration failed`);
+    });
+}
+
 import Alpine from 'alpinejs';
 
 import $ from 'jquery';
--- cadana/resources/views/layouts/app.blade.php
+++ cadana/resources/views/layouts/app.blade.php
@@ -5,7 +5,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
+        <meta name="apple-mobile-web-app-status-bar" content="#01d679">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+
         <title>App | Cadana Fertility</title>
+
+        <link rel="manifest" href="/manifest.json" />
 
         <!-- fontawesome cdn -->
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
